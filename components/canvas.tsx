eae297c4f46a12c68ebd6cb55b9b03b69ca4bc2d
flatypus <yhc3141@gmail.com>
2022-08-20 09:50:08 -0700

    bruh

--- components/canvas.tsx
+++ components/canvas.tsx
@@ -160,34 +160,3 @@ export default function Canvas() {
     ></canvas>
   );
 }
-
-const Counter = () => {
-  // Use useRef for mutable variables that we want to persist
-  // without triggering a re-render on their change
-  const requestRef: any = useRef();
-  const canvasRef: any = useRef();
-  const previousTimeRef = useRef(null);
-
-  const animate = (time) => {
-    if (previousTimeRef.current != undefined) {
-      // Pass on a function to the setter of the state
-      // to make sure we always have the latest state\
-      // do the animation
-    }
-    previousTimeRef.current = time;
-    requestRef.current = requestAnimationFrame(animate);
-  };
-
-  useEffect(() => {
-    requestRef.current = requestAnimationFrame(animate);
-    return () => cancelAnimationFrame(requestRef.current);
-  }, []); // Make sure the effect runs only once
-
-  return (
-    <canvas
-      id="canvas"
-      ref={canvasRef}
-      className="w-screen min-h-screen"
-    ></canvas>
-  );
-};
