dd99f9f0bcb363dc1f4f02a6eb30017926f5734f
flatypus <yhc3141@gmail.com>
2022-10-14 22:26:06 -0700

    theoretically less flickering

--- components/appear.tsx
+++ components/appear.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Transition } from '@headlessui/react'
 import { useInView } from 'react-intersection-observer';
 
@@ -7,6 +7,12 @@ export default function Appear({ children, delay = 0, enterTo = "translate-x-0 o
         threshold: 0,
         triggerOnce: true
     });
+    const [show, setShow] = useState(false);
+    useEffect(() => {
+        setTimeout(() => {
+            setShow(true)
+        }, 100)
+    }, [])
     return (
         <>
             <Transition show={inView} appear={true}>
@@ -16,10 +22,10 @@ export default function Appear({ children, delay = 0, enterTo = "translate-x-0 o
                     leave={`transition-opacity duration-[${duration}ms]`}
                     leaveFrom={`translate-x-0 opacity-100`}
                     leaveTo={`translate-x-0 opacity-100`}>
-                    <div>{children}</div>
+                    <div className={!show ? "opacity-0" : ""}>{children}</div>
                 </Transition.Child>
             </Transition>
-            <div ref={ref} />
+            <div ref={ref}></div>
         </>
     )
 }
